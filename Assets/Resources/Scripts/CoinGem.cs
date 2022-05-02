8a99c47c4cb8a3a3ec6c96d72c9a7f964e3aa2e1
wongdayou <wongdayou@gmail.com>
2022-05-02 11:21:40 +0800

    first commit

---
+++ Assets/Resources/Scripts/CoinGem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinGem : MonoBehaviour {
+
+	public int type = 1; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// despawn coin if it goes past the left edge of the screen
+		if (transform.position.x < -25) {
+			Destroy(gameObject);
+		}
+		else {
+
+			// ensure coin moves at the same rate as the skyscrapers do
+			transform.Translate(-SkyscraperSpawner.speed * Time.deltaTime, 0, 0, Space.World);
+		}
+
+		// infinitely rotate this coin about the Y axis in world space
+		transform.Rotate(0, 5f, 0, Space.World);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (type == 1) {
+			// trigger coin pickup function if a helicopter collides with this
+			other.transform.parent.GetComponent<HeliController>().PickupCoin();
+		}
+		else{
+			// if prefab is a gem trigger gem pickup function
+			other.transform.parent.GetComponent<HeliController>().PickupGem();
+		}
+		
+		Destroy(gameObject);
+	}
+}
